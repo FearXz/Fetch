dde71563980a0f4a79740699403474550daa8807
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-12-06 17:11:53 +0100

    da fixare

--- assets/js/addEventDeleteBtn.js
+++
@@ -1,9 +0,0 @@
-let addEventDeleteBtn = function () {
-  let deleteButtonNode = document.querySelectorAll(".deleteBtn");
-  deleteButtonNode.forEach((button, index) => {
-    button.addEventListener("click", (event) => {
-      event.currentTarget.parentNode.parentNode.remove();
-      console.log(registeredPet.splice(index, 1));
-    });
-  });
-};
